e3dab9ef3689e02dd3244a1d84674def0b5c3b7d
MorTrof <glebtrofimow@gmail.com>
2026-02-16 19:24:07 +0700

    package list added

---
+++ temp.py
@@ -0,0 +1,6 @@
+import requests
+import subprocess
+import os
+
+#conn = subprocess.run(["powershell", "-Command", 'choco find --source="\\192.168.1.170\share\learner" '], check=True)
+print(os.listdir("\\\\192.168.1.170\\share\\"))
